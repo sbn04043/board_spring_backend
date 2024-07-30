04f317f1c924987b33a185294c68787a0a64646b
sbn04043 <sbn04043@gmail.com>
2024-07-30 20:53:28 +0900

    board reate read update

--- src/main/java/com/nc13/react_board/Controller/BoardController.java
+++ src/main/java/com/nc13/react_board/Controller/BoardController.java
@@ -2,8 +2,13 @@ package com.nc13.react_board.Controller;
 
 import com.nc13.react_board.Service.BoardService;
 import com.nc13.react_board.model.BoardDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -17,7 +22,75 @@ public class BoardController {
 
     @GetMapping("showOne/{id}")
     public BoardDTO showOne(@PathVariable int id) {
-        System.out.println(BOARD_SERVICE.selectOne(id));
         return BOARD_SERVICE.selectOne(id);
+    }
+
+    @GetMapping("showList/{pageNo}")
+    public HashMap<String, Object> selectList(@PathVariable int pageNo) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        int maxPage = BOARD_SERVICE.selectMaxPage();
+        if (maxPage < 1)
+            maxPage = 1;
+
+        int startPage, endPage;
+        if (maxPage < 6) {
+            startPage = 1;
+            endPage = maxPage;
+        } else if (pageNo < 4) {
+            startPage = 1;
+            endPage = 5;
+        } else if (pageNo > maxPage - 3) {
+            startPage = maxPage - 4;
+            endPage = maxPage;
+        } else {
+            startPage = pageNo - 2;
+            endPage = pageNo + 2;
+        }
+
+        resultMap.put("startPage", startPage);
+        resultMap.put("endPage", endPage);
+        resultMap.put("maxPage", maxPage);
+        resultMap.put("curPage", pageNo);
+        resultMap.put("boardList", BOARD_SERVICE.selectAll(pageNo));
+
+        return resultMap;
+    }
+
+    @PostMapping("write")
+    public HashMap<String, Object> write(@RequestBody BoardDTO board) {
+
+        board.setWriterId(2);
+        System.out.println(board);
+
+        HashMap<String, Object> resultMap = new HashMap<>();
+        try {
+            BOARD_SERVICE.insert(board);
+            resultMap.put("result", "success");
+            resultMap.put("resultId", board.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultMap.put("result", "fail");
+        }
+
+        return resultMap;
+    }
+
+    @PostMapping("update")
+    public HashMap<String, Object> update(@RequestBody BoardDTO board) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+
+        BOARD_SERVICE.update(board);
+
+        resultMap.put("destId", board.getId());
+
+        return resultMap;
+    }
+
+    @PostMapping("delete")
+    public HashMap<String, Object> delete(@RequestBody int id) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        System.out.println(id);
+
+        return resultMap;
     }
 }
--- src/main/java/com/nc13/react_board/Service/BoardService.java
+++ src/main/java/com/nc13/react_board/Service/BoardService.java
@@ -5,10 +5,14 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class BoardService {
     private final SqlSession SESSION;
     private final String NAMESPACE = "mapper.BoardMapper";
+    private final int PAGE_SIZE = 20;
 
     @Autowired
     public BoardService(SqlSession session) {
@@ -17,5 +21,25 @@ public class BoardService {
 
     public BoardDTO selectOne(int id) {
         return SESSION.selectOne(NAMESPACE + ".selectOne", id);
+    }
+
+    public List<BoardDTO> selectAll(int pageNo) {
+        HashMap<String, Integer> paramMap = new HashMap<>();
+        paramMap.put("startRow", (pageNo - 1) * PAGE_SIZE);
+        paramMap.put("size", PAGE_SIZE);
+        return SESSION.selectList(NAMESPACE + ".selectList", paramMap);
+    }
+
+    public int selectMaxPage() {
+        int maxPage = SESSION.selectOne(NAMESPACE + ".selectMaxPage");
+        return maxPage % PAGE_SIZE == 0 ? maxPage / PAGE_SIZE : maxPage / PAGE_SIZE + 1;
+    }
+
+    public void insert(BoardDTO board) {
+        SESSION.insert(NAMESPACE + ".insert", board);
+    }
+
+    public void update(BoardDTO board) {
+        SESSION.update(NAMESPACE + ".update", board);
     }
 }
